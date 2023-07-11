9b200d25f54e70feb9a1c31bfb5e2a394023a626
sbunkerchris <50494698+sbunkerchris@users.noreply.github.com>
2023-07-10 21:20:41 -0600

    W04_finish

--- W04_Assignment/cms/src/app/documents/document.model.ts
+++ W04_Assignment/cms/src/app/documents/document.model.ts
@@ -1,9 +1,9 @@
 export class Document {
-    constructor(
-      public id: string,
-      public name: string,
-      public description: string,
-      public url: string,
-      public children?: Document[]
-    ) {}
-  }
+  constructor(
+    public id: string,
+    public name: string,
+    public description: string,
+    public url: string,
+    public children?: Document[]
+  ) {}
+}
